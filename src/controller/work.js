b3c1de6f7d5af6b5725282e9df5ea7bb493233f2
Piyush Sethiya <piyushseth226@gmail.com>
2024-09-27 22:57:17 +0530

    password hashing

--- src/controller/work.js
+++ src/controller/work.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const app = express();
 const db = require("../config/db.js");
-const person = require("../model/person.js");
+const passport = require('../config/auth.js')
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
 const personRoutes = require("../routes/PersonRoutes.js");
 const menuRoutes = require("../routes/MenuRoutes.js");
-
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); // middleware
@@ -23,35 +19,18 @@ const logRequest = (req, res, next) => {
 };
 app.use(logRequest); // middleware
 
-passport.use(
-  new LocalStrategy(async (USERNAME, PASSWORD, done) => {
-    try {
-      console.log("Received Credentials: ", USERNAME, PASSWORD);
-      const user = await person.findOne({ username: USERNAME });
-      if (!user) {
-        return done(null, false, { message: "Incorrect Username." });
-      }
-      const isPasswordMatch = user.passport === PASSWORD ? true : false;
-      if (isPasswordMatch) {
-        return done(null, user);
-      }else{
-        return done (null, false, {message: 'password incorrect.'});
-      }
-    } catch (err) {
-      return done(err);
-    }
-  })
-);
 
 app.use(passport.initialize());
-const auth = passport.authenticate('local', {session: false});
+const auth = passport.authenticate("local", { session: false });
 
-app.get("/", auth, function (req, res) {
+// routes
+app.get("/", function (req, res) {
   res.send("Hello, Welcome node js api for curd operation..");
 });
 app.use("/person", personRoutes);
-app.use("/menu", menuRoutes);
+app.use("/menu", auth, menuRoutes);
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server is running port " + PORT);
 });
