349d143e02b4aa940fedd441014fe36b69fbfc30
Piyush Sethiya <piyushseth226@gmail.com>
2024-09-29 18:45:01 +0530

    add port

--- src/controller/work.js
+++ src/controller/work.js
@@ -30,6 +30,7 @@ app.get("/", function (req, res) {
 });
 app.use("/person", auth, personRoutes);
 app.use("/menu", auth, menuRoutes);
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server is running port " + PORT);
 });
